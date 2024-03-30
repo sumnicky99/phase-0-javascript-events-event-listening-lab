ba590c87ac8054061920915413053f0690b7d108
Sumnicky99 <sumnicky99@gmail.com>
2024-03-30 12:40:07 +0300

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+
 function addingEventListener() {
+
+    const input = document.getElementById('button');
+
+    input.addEventListener('click', function() {
+        alert('I was clicked!');
+});
+
 }
+
+addingEventListener()
